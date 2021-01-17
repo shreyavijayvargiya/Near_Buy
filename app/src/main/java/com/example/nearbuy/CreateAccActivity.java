4ad0662a33a40c537f0cceba99815564cc04cc93
shreyavijayvargiya <shreyavijayvargiya16@gmail.com>
2021-01-17 16:41:41 +0530

    database

--- app/src/main/java/com/example/nearbuy/CreateAccActivity.java
+++ app/src/main/java/com/example/nearbuy/CreateAccActivity.java
@@ -3,21 +3,21 @@ package com.example.nearbuy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 
 
 public class CreateAccActivity extends AppCompatActivity {
@@ -25,9 +25,13 @@ public class CreateAccActivity extends AppCompatActivity {
     private EditText editTextMail;
     private EditText editTextPhone;
     private EditText editTextAddress;
-    Button createbtn = findViewById(R.id.create);
+    private Button createbtn;
 
     private DatabaseReference mUserDatabaseReference;
+    private FirebaseDatabase database;
+    private  FirebaseAuth mAuth;
+    private static final String USER= "user";
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,76 +45,80 @@ public class CreateAccActivity extends AppCompatActivity {
         editTextMail = findViewById(R.id.mail);
         editTextPhone = findViewById(R.id.phone);
         editTextAddress = findViewById(R.id.address);
+        createbtn = findViewById(R.id.create);
 
-        mUserDatabaseReference = mFirebaseDatabase.getReference().child("users");
+        database= FirebaseDatabase.getInstance();
+        mUserDatabaseReference = database.getReference("USER");
+        mAuth= FirebaseAuth.getInstance();
 
         createbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String name = editTextName.getText().toString().trim();
                 String mail = editTextMail.getText().toString().trim();
                 String phone = editTextPhone.getText().toString().trim();
                 String address = editTextAddress.getText().toString().trim();
-
-                User user = new User(name, mail, phone, address);
+                validateInputs(name, mail, phone, address);
+                user = new User(name, mail, phone, address);
                 mUserDatabaseReference.push().setValue(user);
+                registerUser(name, mail);
             }
         });
     }
 
-    private boolean validateInputs(String name, String mail, String phone, String address) {
+    public void registerUser(String name, String mail){
+        mAuth.createUserWithEmailAndPassword(name, mail)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(CreateAccActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    public void updateUI(FirebaseUser currentUser){
+        String keyId= mUserDatabaseReference.push().getKey();
+        mUserDatabaseReference.child(keyId).setValue(user);
+        Intent i = new Intent(this, ProfileFragment.class);
+        startActivity(i);
+
+    }
+
+    private void validateInputs(String name, String mail, String phone, String address) {
         if (name.isEmpty()) {
             editTextName.setError("Name required");
             editTextName.requestFocus();
-            return true;
+            return;
         }
 
         if (mail.isEmpty()) {
             editTextMail.setError("Mail ID required");
             editTextMail.requestFocus();
-            return true;
+            return;
         }
 
         if (phone.isEmpty()) {
             editTextPhone.setError("Phone required");
             editTextPhone.requestFocus();
-            return true;
+            return;
         }
 
         if (address.isEmpty()) {
             editTextAddress.setError("Address required");
             editTextAddress.requestFocus();
-            return true;
         }
 
 
-        return false;
     }
 
-
-
-        //if (!validateInputs(name, mail, phone, address)) {
-
-            // CollectionReference dbProducts = db.collection("User");
-
-            // User user = new User(
-            //       name,
-            //     mail,
-            //   phone,
-            // address
-            //);
-            // dbProducts.add(user)
-            // .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            // @Override
-            //public void onSuccess(DocumentReference documentReference) {
-            // Toast.makeText(CreateAccActivity.this, "Product Added", Toast.LENGTH_LONG).show();
-            //}
-            //})
-            //.addOnFailureListener(new OnFailureListener() {
-            //   @Override
-            // public void onFailure(@NonNull Exception e) {
-            //Toast.makeText(CreateAccActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-            // }
-            // });
-
-        }
+}
