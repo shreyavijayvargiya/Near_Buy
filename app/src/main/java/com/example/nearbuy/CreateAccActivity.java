7f155eff243db10161eca0ccb69e6be4e870c884
shreyavijayvargiya <shreyavijayvargiya16@gmail.com>
2021-01-17 14:57:42 +0530

    create acc

--- app/src/main/java/com/example/nearbuy/CreateAccActivity.java
+++ app/src/main/java/com/example/nearbuy/CreateAccActivity.java
@@ -5,40 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 
 
-
-public class CreateAccActivity<FirebaseFirestore> extends AppCompatActivity implements View.OnClickListener {
+public class CreateAccActivity extends AppCompatActivity {
     private EditText editTextName;
     private EditText editTextMail;
     private EditText editTextPhone;
     private EditText editTextAddress;
+    Button createbtn = findViewById(R.id.create);
 
-
-    private DatabaseReference db;
+    private DatabaseReference mUserDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
 
-        editTextName = findViewById(R.id.name);
-        editTextMail=findViewById(R.id.mail);
-        editTextPhone=findViewById(R.id.phone);
-        editTextAddress=findViewById(R.id.address);
-        findViewById(R.id.create).setOnClickListener(this);
-        db = FirebaseDatabase.getInstance().getReference();
+        FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
 
+        editTextName = findViewById(R.id.name);
+        editTextMail = findViewById(R.id.mail);
+        editTextPhone = findViewById(R.id.phone);
+        editTextAddress = findViewById(R.id.address);
+
+        mUserDatabaseReference = mFirebaseDatabase.getReference().child("users");
+
+        createbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString().trim();
+                String mail = editTextMail.getText().toString().trim();
+                String phone = editTextPhone.getText().toString().trim();
+                String address = editTextAddress.getText().toString().trim();
+
+                User user = new User(name, mail, phone, address);
+                mUserDatabaseReference.push().setValue(user);
+            }
+        });
     }
+
     private boolean validateInputs(String name, String mail, String phone, String address) {
         if (name.isEmpty()) {
             editTextName.setError("Name required");
@@ -69,35 +88,29 @@ public class CreateAccActivity<FirebaseFirestore> extends AppCompatActivity impl
     }
 
 
-    @Override
-    public void onClick(View view) {
-        String name = editTextName.getText().toString().trim();
-        String mail = editTextMail.getText().toString().trim();
-        String phone = editTextPhone.getText().toString().trim();
-        String address = editTextAddress.getText().toString().trim();
-        if (!validateInputs(name, mail, phone, address)) {
 
-          // CollectionReference dbProducts = db.collection("User");
+        //if (!validateInputs(name, mail, phone, address)) {
 
-            User user = new User(
-                    name,
-                    mail,
-                    phone,
-                    address
-            );
-           // dbProducts.add(user)
-                   // .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                       // @Override
-                        //public void onSuccess(DocumentReference documentReference) {
-                           // Toast.makeText(CreateAccActivity.this, "Product Added", Toast.LENGTH_LONG).show();
-                        //}
-                    //})
-                    //.addOnFailureListener(new OnFailureListener() {
-                     //   @Override
-                       // public void onFailure(@NonNull Exception e) {
-                            //Toast.makeText(CreateAccActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                       // }
-                   // });
+            // CollectionReference dbProducts = db.collection("User");
 
-    }
-}}
+            // User user = new User(
+            //       name,
+            //     mail,
+            //   phone,
+            // address
+            //);
+            // dbProducts.add(user)
+            // .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            // @Override
+            //public void onSuccess(DocumentReference documentReference) {
+            // Toast.makeText(CreateAccActivity.this, "Product Added", Toast.LENGTH_LONG).show();
+            //}
+            //})
+            //.addOnFailureListener(new OnFailureListener() {
+            //   @Override
+            // public void onFailure(@NonNull Exception e) {
+            //Toast.makeText(CreateAccActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            // }
+            // });
+
+        }
--- app/src/main/java/com/example/nearbuy/User.java
+++ app/src/main/java/com/example/nearbuy/User.java
@@ -17,18 +17,28 @@ private String name, phone , mail ,address;
     public String getName() {
         return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getMail() {
         return mail;
     }
 
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
     public String getPhone() {
         return phone;
     }
-
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
     public String getAddress() {
         return address;
     }
-
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
 
