e56688ca5ead2f88c1c2367b739db9cab1866a24
shreyavijayvargiya <shreyavijayvargiya16@gmail.com>
2021-01-18 01:09:56 +0530

    final

--- app/src/main/java/com/example/nearbuy/Vendor_order.java
+++ app/src/main/java/com/example/nearbuy/Vendor_order.java
@@ -2,6 +2,7 @@ package com.example.nearbuy;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -15,7 +16,13 @@ public class Vendor_order extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vendor_order);
 
-
+        Button button= (Button) findViewById(R.id.yatra);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.vendor_map);
+            }
+        });
 
 
 
